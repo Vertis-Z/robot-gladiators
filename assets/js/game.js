80875aaf2c211bac5eef0aa1962f324662c6db91
Vertis-Z <48894365+Vertis-Z@users.noreply.github.com>
2022-04-06 01:19:08 -0400

    first commit

---
+++ assets/js/game.js
@@ -0,0 +1,17 @@
+var playerName = window.prompt("What is your robots name?");
+// Note the lack of quotation marks around the playerName
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+// this create a function named "fight"
+
+function fight () {
+    window.alert("the fight has begun!");
+}
+
+// fight();
